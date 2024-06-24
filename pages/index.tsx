70d97415c0879b2c1903fde4c7e149c87d09a710
rjk79 <robertku79@gmail.com>
2024-06-24 14:42:09 -0400

    Add prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,39 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Menu } from '@headlessui/react';
 import classNames from 'classnames';
+import prisma from '../lib/prisma';
+import { GetStaticProps } from 'next';
 
-function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    // where: { published: true },
+    include: {
+      author: {
+        select: { name: true }, // returns specified fields
+      },
+    },
+  });
+  return {
+    props: { feed },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10,
+  };
+};
+
+function Home(props) {
   const router = useRouter();
-
+  console.log(props.feed);
+  
   return (
     <div className="flex flex-col gap-2 items-start">
       <div>Home</div>
+      <div>
+        {props?.feed.map((post, index) => (
+          <div key={index}>{post.title}</div>
+        ))}
+      </div>
       <Menu>
         <Menu.Button className="border border-black">Pages</Menu.Button>
         <Menu.Items className="bg-red-200">
